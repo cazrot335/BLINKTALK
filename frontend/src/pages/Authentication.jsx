913321f80d8837b90472a54fee7f2008d9b9bfd4
pk400 <parthkamat10@gmail.com>
2024-03-16 14:39:26 +0530

    added deploy

--- frontend/src/pages/Authentication.jsx
+++ frontend/src/pages/Authentication.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
-import { auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithGoogle } from 'backend/authConfig';
+import { auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithGoogle } from './authConfig'
 import { useNavigate } from 'react-router-dom';
 
 // Styled Components for Toggle Button
